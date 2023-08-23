2d4fc7e1d5c8bd3b9f55880df8a7e4365f463139
Rendika <142858681+rendika2@users.noreply.github.com>
2023-08-23 08:30:59 +0700

    upload code 1-15

---
+++ 4.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+<?php
+    /*
+    Komentar
+    beberapa
+    baris
+    */
+    ?>
+</body>
+</html>
---
+++ 5.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $txt= "SMKN 1 Jenangan";
+    echo "i love $txt";
+    ?>
+</body>
+</html>
